da90d9865a0a7656601cdf57068b47409fe1cbca
Brian Ting <brian.t.ting@gmail.com>
2016-10-30 14:45:32 -0400

    added the gem devise for logins followed the readme up until rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :models
+  devise_for :models
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
