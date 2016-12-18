242a6701894bc20f582a2b27ee17c077526484b1
Alex Fiuk <afiuk@seas.upenn.edu>
2016-12-17 20:41:45 -0500

    more config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'artifact-tracker.herokuapp.com' }
+  # Do care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'artifact.tracker@gmail.com',
+    password:             'derpderp',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
